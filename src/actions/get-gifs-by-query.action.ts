144d86716d9d5bcd7f146b17b7b0a1d8825876e7
Carlos Ramos <carlosramosdev@protonmail.com>
2026-05-19 14:53:04 +0200

    refactored axios api call

--- src/actions/get-gifs-by-query.action.ts
+++ src/actions/get-gifs-by-query.action.ts
@@ -1,24 +1,20 @@
-import axios from 'axios';
 import type { GiphyResponse } from '../interfaces/giphy.response';
 import type { Gif } from '../interfaces/gif.interface';
+import { giphyApi } from '../api/Giphy.api';
 
-const url = 'https://api.giphy.com/v1/gifs/search';
-
-export const getGifsByQuery = async (query: string): Promise<Gif[]> => {  
-  const response = await axios.get<GiphyResponse>(url, {
+export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
+  const response = await giphyApi<GiphyResponse>("/search", {
     params: {
-      api_key: import.meta.env.VITE_GYPHY_API_KEY,
       q: query,
       limit: 5,
-      lang: 'en',      
     },
   });
 
-  return response.data.data.map(( gif ) => ({
+  return response.data.data.map((gif) => ({
     id: gif.id,
     title: gif.title,
     url: gif.images.original.url,
     width: Number(gif.images.original.width),
-    height: Number(gif.images.original.height)
-  }))
+    height: Number(gif.images.original.height),
+  }));
 };
